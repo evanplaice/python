b5b6458c6a34959d68bf84dc444ec461ec3c34e8
Evan Plaice <evanplaice@gmail.com>
2026-03-17 14:29:56 -0600

    WIP

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+# div > add
+print("Hens", 25 + 30 / 6)
+# 25 + 5
+# mult > mod > add/sub
+print("Roosters", 100 - 25 * 3 % 4)
+# 100 - 75 % 4
+# 100 - 3
+print("Now I will count the eggs:")
+# mod > div > add/sub 
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# 3 + 2 + 1 - 5 + 0 - 1 / 4 + 6
+# 3 + 2 + 1 - 5 + 0 - .25 + 6
+print("Is it true that 3 + 2 < 5 - 7?")
+# add/sub > lt
+print(3 + 2 < 5 - 7)
+# 5 < -2
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.")
+print("How about some more.")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
